d1d1303e775fe88205fa450f4126a3fd9a6bc621
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-05-30 13:15:48 -0400

    Conexión con la base de datos y muestra de los registros

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("prueba")
+import pandas as pd
+from sqlite3 import connect
+conexion = connect("db_personas.db")
+
+df = pd.read_sql('SELECT fecha_ingreso AS Fecha, Rol, residencia as Residencia, rut AS RUT, nombre_completo AS "Nombre Completo", \
+                 nacionalidad AS Nacionalidad, fecha_de_nacimiento AS "Fecha de Nacimiento", profesion AS Profesion, sueldo AS Sueldo \
+                 FROM Salarios AS sal \
+                 INNER JOIN personas AS pe \
+                 ON sal.id_salarios = pe.id_rol;',conexion)
+
+print(df)
