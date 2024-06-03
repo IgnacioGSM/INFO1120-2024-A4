f47f035476fc19a57af94ab18b4398f1767dbf90
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-06-03 08:27:55 -0400

    Definición de un dataframe con todos los datos de ambas tablas unidos con inner join

--- test.py
+++ test.py
@@ -2,10 +2,7 @@ import pandas as pd
 from sqlite3 import connect
 conexion = connect("db_personas.db")
 
-df = pd.read_sql('SELECT fecha_ingreso AS Fecha, Rol, residencia as Residencia, rut AS RUT, nombre_completo AS "Nombre Completo", \
-                 nacionalidad AS Nacionalidad, fecha_de_nacimiento AS "Fecha de Nacimiento", profesion AS Profesion, sueldo AS Sueldo \
-                 FROM Salarios AS sal \
-                 INNER JOIN personas AS pe \
-                 ON sal.id_salarios = pe.id_rol;',conexion)
+df = pd.read_sql("SELECT * from Salarios INNER JOIN personas ON Salarios.id_salarios = personas.id_rol",conexion)
+
 
 print(df)
