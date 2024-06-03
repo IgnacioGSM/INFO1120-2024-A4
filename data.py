6c73d66093f89d2c4584f83bdf981bdff5673d4a
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-06-03 10:14:18 -0400

    testeo de filtración de datos

--- data.py
+++ data.py
@@ -7,13 +7,21 @@ def get_database(file):
     db = pd.read_sql("SELECT * from Salarios INNER JOIN personas ON Salarios.id_salarios = personas.id_rol",conexion)
     return db
 
-def filtro_singular(dataframe="df",columna="rut", condicion="11316802-1"):
+def filtro(dataframe="df",columna="rut", condicion="11316802-1"):
     dato = dataframe[dataframe[columna] == condicion]
     return dato
+
+
 
 df = get_database("db_personas.db")
 
 
-hola = filtro_singular(df,"rut","1518146-K")
+hola = filtro(df,"rut","1518146-K")
 
-print(hola)
+multiples = filtro(df,"nacionalidad","Chilena")
+
+for i in multiples.index.values:
+    print(df.iloc[i])
+    print()
+
+#print(df.iloc[hola.index.values[0]])     # devuelve los datos de una sola fila
