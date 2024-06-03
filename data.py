c2d2a0cfd58cda1d812db4bec5d3f7f0d5464984
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-06-03 09:23:57 -0400

    creada función "get_database"

--- data.py
+++ data.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from sqlite3 import connect
-conexion = connect("db_personas.db")
 
-df = pd.read_sql("SELECT * from Salarios INNER JOIN personas ON Salarios.id_salarios = personas.id_rol",conexion)
+def get_database(file):
+    conexion = connect(file)
 
+    db = pd.read_sql("SELECT * from Salarios INNER JOIN personas ON Salarios.id_salarios = personas.id_rol",conexion)
+    return db
 
-print(df)
+hola = get_database("db_personas.db")
+
+print(hola)
