82d573cfc32c3907df13389ace20baa55cd0ade7
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-06-09 17:16:41 -0400

    Ajustes en la importacion de funciones

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import entradas as ent
+from entradas import *
 from data import *
 
 
@@ -15,17 +15,17 @@ columnas_seleccion_multiple = {
 }
 
 print("1- Un solo documento\n2- Multiples documentos")
-seleccion_numero_documentos = ent.seleccion_opciones(2)
+seleccion_numero_documentos = seleccion_opciones(2)
 
 if seleccion_numero_documentos == 1:
     print("Escoja un criterio por el que buscar:")
     for key,value in columnas_seleccion_unica.items():
         print(f"{key}- {value}")
-    columna_elegida = ent.seleccion_opciones(len(columnas_seleccion_unica))
+    columna_elegida = seleccion_opciones(len(columnas_seleccion_unica))
     subDF = filtro(df,columnas_seleccion_unica[columna_elegida])
     print(subDF)
     print()
-    condicion = ent.entrada_indice(0,len(subDF),"Ingrese el indice del dato deseado: ")
+    condicion = entrada_indice(0,len(subDF),"Ingrese el indice del dato deseado: ")
     subDF = df[df.index == condicion]
     print(subDF)
     subDF.reset_index(inplace=True)
@@ -36,19 +36,19 @@ else:
     print("Escoja un criterio por el que buscar:")
     for key,value in columnas_seleccion_multiple.items():
         print(f"{key}- {value}")
-    columna_elegida = ent.seleccion_opciones(len(columnas_seleccion_multiple))
+    columna_elegida = seleccion_opciones(len(columnas_seleccion_multiple))
     opciones = sorted(list(set(df[columnas_seleccion_multiple[columna_elegida]])))
     for i in range(len(opciones)):
         print(f"{i+1}- {opciones[i]}")
     print()
-    condicion = ent.seleccion_opciones(len(opciones))
+    condicion = seleccion_opciones(len(opciones))
     condicion = opciones[condicion-1]
     subDF = filtro(df,columnas_seleccion_multiple[columna_elegida],condicion)
     subDF.reset_index(inplace=True,drop=True)
     print(subDF)
     print("\nEscoja el rango: ")
-    inf = ent.entrada_indice(0,len(subDF),"Ingrese el limite inferior: ")
-    sup = ent.entrada_indice(inf,len(subDF),"Ingrese el limite superior: ")
+    inf = entrada_indice(0,len(subDF),"Ingrese el limite inferior: ")
+    sup = entrada_indice(inf,len(subDF),"Ingrese el limite superior: ")
     subDF = subDF.iloc[inf:sup+1]
     subDF.reset_index(inplace=True)
     print(subDF)
