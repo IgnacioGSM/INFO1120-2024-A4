acb029e9540264306343ec05441cce39186c7f6a
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-06-08 20:51:14 -0400

    extraido el sueldo promedio de cada profesión

--- promedio_sueldos.py
+++ promedio_sueldos.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as mpl
-from data import get_database, filtro
+from data import get_database
+from numpy import mean
 
 df = get_database("db_personas.db")
 
 profesiones = sorted(list(set(df["profesion"])))
 
-print(profesiones)
+sueldo_promedio = []
+for pro in profesiones:
+    sueldo_promedio.append(mean(df[df["profesion"] == pro]["Sueldo"]))
+
+print(sueldo_promedio)
