f1d070120c7be94246bf4aa0e32168bfad423d12
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-06-09 14:03:34 -0400

    ajustes visuales del grafico

--- promedio_sueldos.py
+++ promedio_sueldos.py
@@ -10,6 +10,9 @@ sueldo_promedio = []
 for pro in profesiones:
     sueldo_promedio.append(mean(df[df["profesion"] == pro]["Sueldo"]))
 
-plt.bar(profesiones,sueldo_promedio)
+plt.figure(figsize=(12,5))
+plt.bar(profesiones,sueldo_promedio, 0.4)
 plt.ylabel("Sueldo promedio (Millones CLP)")
+plt.xticks(rotation=45)
+plt.subplots_adjust(bottom=0.45)
 plt.show()
