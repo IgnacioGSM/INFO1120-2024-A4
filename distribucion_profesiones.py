cd009b29adeaf26f18b2cc77474f3cd7febc77b0
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-06-09 16:36:32 -0400

    cantidad de personas en cada profesion

--- distribucion_profesiones.py
+++ distribucion_profesiones.py
@@ -1,2 +1,11 @@
 import matplotlib.pyplot as plt
 from data import get_database
+
+df = get_database("db_personas.db")
+
+profesiones = sorted(list(set(df["profesion"])))
+
+distribucion = []
+for pro in profesiones:
+    distribucion.append(len(df[df["profesion"] == pro]))
+print(distribucion)
